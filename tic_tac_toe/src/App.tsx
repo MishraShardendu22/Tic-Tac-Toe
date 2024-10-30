1522bd64b43718a0386cc24905e1329590dbce60
Shardendu Mishra <shardendumishra01@gmail.com>
2024-10-31 01:09:43 +0530

    Initialised Typescript Front and Backend

--- tic_tac_toe/src/App.tsx
+++ tic_tac_toe/src/App.tsx
@@ -1,9 +1,9 @@
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-red-700">Hello Word</h1>
+    <div className="bg-gradient-to-r from-purple-800 via-violet-800 to-indigo-800 min-h-screen flex items-center justify-center">
+        <h1 className="text-3xl text-white">Hello World</h1>
     </div>
   )
 }
 
-export default App
+export default App;
